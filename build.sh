7ac858104ecb828809aad10fc572fcd435575de4
jreyes-13 <jonatan.reyes01@utrgv.edu>
2026-04-13 12:39:20 -0500

    render deployment

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Creates a superuser
+python manage.py createsuperuser --noinput || true 
