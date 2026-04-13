e43c041c02e079c7fba9833fadb1f44e7b3de936
jreyes-13 <jonatan.reyes01@utrgv.edu>
2026-04-13 13:55:48 -0500

    render deployment

--- forms/settings.py
+++ forms/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'formsApp.apps.FormsAppConfig'
+    'formsApp.apps.FormsappConfig'
 ]
 
 MIDDLEWARE = [
