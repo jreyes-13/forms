91c96eb3deab38a682908a3332bdc9621998ec71
jreyes-13 <jonatan.reyes01@utrgv.edu>
2026-04-13 18:19:10 -0500

    render deployment - fixed settings

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-# Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poerty, etc)
 pip install -r requirements.txt
 
-# Convert static asset files 
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
 python manage.py migrate
 
-# Creates a superuser
-python manage.py createsuperuser --noinput || true 
+python manage.py createsuperuser --noinput || true
